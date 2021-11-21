f29464e94c3a91230dc39ec48cf96b0dc730a0e8
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-11-21 17:31:09 +0200

    adding tasks component

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Dentist Appointment',
+        day: 'November 22nd at 10:30 am',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'November 23rd at 1:30 pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Grocery Shopping',
+        day: 'November 24th at 9:30 am',
+        reminder: false,
+    },
+]
